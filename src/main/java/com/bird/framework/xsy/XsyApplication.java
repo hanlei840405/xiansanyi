4b9ae770fdbf63ee9bbfd7c1e3447a02733f5b2f
JesseHan <hanlei.840405@gmail.com>
2018-06-04 21:19:47 +0800

    init

---
+++ src/main/java/com/bird/framework/xsy/XsyApplication.java
@@ -0,0 +1,12 @@
+package com.bird.framework.xsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XsyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XsyApplication.class, args);
+	}
+}
